2afbbc696afee1d3b9cc068d31fde73ee91aa0db
Jenam <kumarjenam31@gmail.com>
2025-10-06 18:12:49 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const allowedOrigins = [
-  "http://localhost:8000",
-  "https://career-compass-3cg1.onrender.com/", // Replace with your actual frontend URL
-];
+// const allowedOrigins = [
+//   "http://localhost:8000",
+//   "https://career-compass-3cg1.onrender.com/", // Replace with your actual frontend URL
+// ];
 const api = axios.create({
   baseURL: "https://career-compass-3cg1.onrender.com/",
   withCredentials: true, // ⬅️ crucial for cookies
